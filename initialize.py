90a5e5ef8ccc704e690aec6ed3dc92980a7ef392
gesoges0 <elasticnet1995@gmail.com>
2022-08-14 22:58:38 +0900

    fix: fmt

--- initialize.py
+++ initialize.py
@@ -1,6 +1,6 @@
 import time
-from typing import Any
 from dataclasses import dataclass, field
+from typing import Any
 
 import MySQLdb
 
@@ -10,14 +10,14 @@ def sleep(seconds: int) -> None:
 
 
 @dataclass
-class DB:
+class DatabaseManager:
     host: str
     user: str
     passwd: str
     db: str
 
     def __post_init__(self):
-       self.connection = MySQLdb.connect(
+        self.connection = MySQLdb.connect(
             host=self.host, user=self.user, passwd=self.passwd, db=self.db
         )
 
@@ -27,12 +27,54 @@ class DB:
             cursor.execute(query)
             return cursor.fetchall()
 
+    def create_table(self, table_name: str, columns: dict[str, str]):
+        """create table"""
+        columns_with_types = [
+            f"{column_name} {data_type}" for column_name, data_type in columns.items()
+        ]
+        self._execute(
+            f"""
+            CREATE TABLE IF NOT EXISTS {table_name}
+            ({', '.join(columns_with_types)});
+            """
+        )
+
+    def add(self, table_name: str, data: dict[str, str]):
+        """insert records"""
+        placeholders = ", ".join("?" * len(data))
+        column_names = ", ".join(data.keys())
+        column_values = tuple(data.values())
+
+        self._execute(
+            f"""
+            INSERT INTO {table_name}
+            ({column_names})
+            VALUES ({placeholders});
+            """,
+            column_values,
+        )
+
+    def select(self, table_name: str, criteria: dict[str, str] = None, order_by=None):
+        """select"""
+        criteria = criteria or {}
+        query = f"SELECT * FROM {table_name}"
+
+        if criteria:
+            placeholders = [f"{column} = ?" for column in criteria.keys()]
+            select_criteria = " AND ".join(placeholders)
+            query += f" WHERE {select_criteria}"
+
+        return self._execute(query, tuple(criteria.values()))
+
+
 if __name__ == "__main__":
-    # MySQLの起動を5秒待つ
+    # MySQLの起動を待つ
     sleep(10)
 
     # DBを初期化
-    db: DB = DB(host="db", user="docker", passwd="docker", db="test_database")
-    records = db._execute("SHOW CREATE DATABASE test_database;")
+    db_manager: DatabaseManager = DatabaseManager(
+        host="db", user="docker", passwd="docker", db="test_database"
+    )
+    records = db_manager._execute("SHOW CREATE DATABASE test_database;")
     for i, record in enumerate(records):
         print(i, record)
