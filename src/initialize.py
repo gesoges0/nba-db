1f8dafa537efa4ab55275d393e27e3ba74ee3376
gesoges0 <elasticnet1995@gmail.com>
2022-08-24 11:47:22 +0900

    fix: player_gamelog

--- src/initialize.py
+++ src/initialize.py
@@ -1,15 +1,23 @@
 import time
-from typing import Any
+from typing import Any, Union
 
+import nba_api.stats.library.parameters
 from nba_api.stats import endpoints as ep
 from nba_api.stats.static import players, teams
 
 from src.db import ActivePlayers, AllPlayers, InactivePlayers, Teams, create_tables, db
 from src.tables import PlayerGameLog
 
+GAMELOG = dict[str, Any]
+
 
 def sleep(seconds: int) -> None:
     time.sleep(seconds)
+
+
+all_players: list[dict[str, Union[int, str]]] = players.get_players()
+active_players: list[dict[str, Union[int, str]]] = players.get_players()
+inactive_players: list[dict[str, Union[int, str]]] = players.get_players()
 
 
 # def initialize(): にしてmainからオプションで実行できるようにする
@@ -50,20 +58,51 @@ def initialize(args):
 def initialize_stats():
     """initialize stats tables"""
 
-    # each player's stats by season
-    # db.db_session.bulk_save_objects(
-    #     [PlayerGameLog(**game_log_by_season_player[0]) for game_log_by_season_player in filter(lambda x: x is not [], map(lambda p: ep.playergamelog.PlayerGameLog(player_id=p["id"]).get_normalized_dict()["PlayerGameLog"], players.get_active_players()[:3]))]
-    # )
+    # 今シーズンの全ゲームログ
 
-    a = []
-    for player_id in map(lambda p: p["id"], players.get_active_players()[:3]):
-        player_game_log: list[dict[str, Any]] = ep.playergamelog.PlayerGameLog(
-            player_id=player_id
-        ).get_normalized_dict()["PlayerGameLog"]
-        if not player_game_log:
-            continue
-        for player_game_log_dict in player_game_log:
-            a.append(PlayerGameLog(**player_game_log_dict))
-    if a:
-        db.db_session.bulk_save_objects(a)
-        db.db_session.commit()
+    # 副作用あり
+    # a = []
+    # for p in players.get_players()[:20]:
+    #     player_game_log: list[dict[str, Any]] = ep.playergamelog.PlayerGameLog(
+    #         player_id=p["id"]
+    #     ).get_normalized_dict()["PlayerGameLog"]
+    #     # if not player_game_log:
+    #     #     continue
+    #     for player_game_log_dict in player_game_log:
+    #         a.append(PlayerGameLog(**player_game_log_dict))
+    # if a:
+    #     db.db_session.bulk_save_objects(a)
+    #     db.db_session.commit()
+
+    # 副作用なし
+    db.db_session.bulk_save_objects(
+        [
+            PlayerGameLog(**player_game_log_dict)
+            for player_game_log_dicts in (
+                ep.playergamelog.PlayerGameLog(player_id=p["id"]).get_normalized_dict()[
+                    "PlayerGameLog"
+                ]
+                for p in active_players[10:20]
+            )
+            for player_game_log_dict in player_game_log_dicts
+        ]
+    )
+    db.db_session.commit()
+
+
+def get_all_season_all_game_logs(season_id: str) -> list[GAMELOG]:
+    return inactive_players
+
+
+def get_current_season_all_game_logs() -> list[GAMELOG]:
+    """今シーズンの全ゲームログ"""
+    return [
+        player_game_log_dict
+        for player_game_log_dicts in (
+            ep.playergamelog.PlayerGameLog(
+                player_id=p["id"],
+            ).get_normalized_dict()["PlayerGameLog"]
+            for p in active_players
+        )
+        for player_game_log_dict in player_game_log_dicts
+    ]
