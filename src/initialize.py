c2e2df691c15e572d610f643a03cecf35a866f94
gesoges0 <elasticnet1995@gmail.com>
2022-09-19 22:58:45 +0900

    fix: black

--- src/initialize.py
+++ src/initialize.py
@@ -76,9 +76,9 @@ def _update_current_season_player_game_log_table() -> None:
 
     # ------------------------------------------------------------------------------------------
     # debug
-    with open('/app/src/team_debug.txt', 'w') as f:
+    with open("/app/src/team_debug.txt", "w") as f:
         for i, player_game_log_dict in enumerate(_get_player_game_log_dicts()):
-            f.write(f'{player_game_log_dict}\n')
+            f.write(f"{player_game_log_dict}\n")
     # ------------------------------------------------------------------------------------------
 
     db.db_session.bulk_save_objects(
